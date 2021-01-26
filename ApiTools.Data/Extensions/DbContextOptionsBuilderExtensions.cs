ad1fdb2507122c6fe4c195ae3c70fb4608bba6fe
Wraptor <wraptor@wheezie.be>
2021-01-27 00:38:49 +0100

    UseDbOptions extension for DbContextOptions

---
+++ ApiTools.Data/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ApiTools.Domain.Options;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ApiTools.Data.Extensions
+{
+    public static class DbContextOptionsBuilderExtensions
+    {
+        public static DbContextOptionsBuilder UseDbOptions(this DbContextOptionsBuilder builder, string connectionString)
+        {
+            ServerVersion serverVersion;
+            if (!ServerVersion.TryFromString(connectionString, out serverVersion))
+            {
+                throw new ArgumentException("Connectionstring is invalid, or the server cannot be determined.");
+            }
+
+            return builder.UseMySql(serverVersion);
+        }
+    }
+}
