5986510a1dd30334c0e571a5ed76a703bc5d628e
Wraptor <wraptor@wheezie.be>
2021-01-31 22:17:00 +0100

    Added ApiTools Exception (for maintainability)

---
+++ ApiTools.Domain/ApiToolsException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApiTools.Domain
+{
+    public abstract class ApiToolsException : Exception
+    {
+        public bool Logged { get; private init; }
+
+        protected ApiToolsException(bool logged = false) : this(null, null, logged)
+        {
+        }
+
+        protected ApiToolsException(string message, bool logged = false) : this(message, null, logged)
+        {
+        }
+
+        protected ApiToolsException(string message, Exception innerException, bool logged = false) : base(message, innerException)
+        {
+            Logged = logged;
+        }
+    }
+}
---
+++ ApiTools.Domain/Exceptions/SmtpException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ApiTools.Domain.Exceptions
+{
+    public class SmtpException : ApiToolsException
+    {
+        public SmtpException(string message, Exception innerException) : base(message, innerException, true)
+        {
+        }
+    }
+}
