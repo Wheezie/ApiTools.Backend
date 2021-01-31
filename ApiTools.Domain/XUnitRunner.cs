389dcba85fcfc93f304648207eb9ee7a878c6630
Wraptor <wraptor@wheezie.be>
2021-01-31 04:02:53 +0100

    XUnitRunner checking function

---
+++ ApiTools.Domain/XUnitRunner.cs
@@ -0,0 +1,23 @@
+﻿#if DEBUG
+using System;
+using System.Reflection;
+
+namespace ApiTools.Domain
+{
+    public static class XUnitRunner
+    {
+        public static bool IsTesting()
+        {
+            foreach (Assembly assem in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (assem.FullName.ToLowerInvariant().Contains("xunit"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+#endif
